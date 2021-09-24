9b6e2644cfd086cb09deff6632e96780136ad8b0
tlw52836 <1660872260@qq.com>
2021-09-24 15:24:46 +0800

    LeetCode习题

--- src/无重复字符的最长子串.java
+++ src/无重复字符的最长子串.java
@@ -1,17 +1,46 @@
 public class 无重复字符的最长子串 {
     public static void main(String[] args) {
-        String s = "pwwkew";
-        System.out.println("不同字符数：" + lengthOfLongestSubstring(s));
+        String s = "bpfbhmipx";
+        System.out.println("最长子串长度：" + lengthOfLongestSubstring(s));
     }
     public static int lengthOfLongestSubstring(String s) {
+        if(s.length() == 0)
+            return 0;
 
-        int count = 0; //用于记录无重复字符的最长子串
-        char[] ch = new char[s.length()];  //用于记录当前扫描川的不同字符，t为数量
-        int t = 0;
+        int len = 1; //用于记录无重复字符的最长子串的长度
+        String str = s.charAt(0) + "";  //用于记录当前扫描串的不同字符，t为数量
+        int t = 1;
+        int i = 0,j = 0;
+        while(i < s.length()){
+            if(j >= s.length())
+                break;
+            j = i + 1;
+           while(j < s.length()){
+                if(str.indexOf(s.charAt(j)) == -1){
+                    str += s.charAt(j);
+                    t++;
+                    j++;
 
-        for(int i = 0;i < s.length();i++){
+                    System.out.println("!=:" + str);
+                }
+                else{
+                    if(t > len){
+                        len = t;
+                    }
+                    System.out.println("==:" + str);
+                    i = str.indexOf(s.charAt(j)) + i + 1;
+                    if(i < s.length()){
+                        str = s.charAt(i) + "";
+                        t = 1;
+                        System.out.println("i" + i);
+                    }
 
+                    break;
+                }
+               if(t > len)
+                   len = t;
+            }
         }
-        return t;
+        return len;
     }
 }
---
+++ src/无重复字符的最长子串2.java
@@ -0,0 +1,14 @@
+public class 无重复字符的最长子串2 {
+    public static void main(String[] args) {
+        String s = "bpfbhmipx";
+        System.out.println("最长子串长度：" + lengthOfLongestSubstring(s));
+    }
+    public static int lengthOfLongestSubstring(String s) {
+        int maxLen = 0;
+        int i = 0,j = 1;
+        while(i < s.length() && j < s.length()){
+
+        }
+        return maxLen;
+    }
+}
