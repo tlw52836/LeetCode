96827cb20927fc07f173629465c7b177cdbaff34
tlw52836 <1660872260@qq.com>
2021-09-29 10:16:18 +0800

    LeetCode习题

--- src/最大子序和.java
+++ src/最大子序和.java
@@ -1,27 +1,22 @@
 public class 最大子序和 {
     public static void main(String[] args) {
-        int[] nums = {-1,0,-2};
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
         System.out.println(maxSubArray(nums));
     }
 
+    /**
+     * 动态规划：最优子结构、重叠子问题
+     * f[i] = max(f[i-1]+num[i],nums[i])  //f[i-1]表示以nums[i-1]为结束点的最大子序列和
+     * @param nums
+     * @return
+     */
+
     public static int maxSubArray(int[] nums) {
-        int maxSum = nums[0];
+        int preMaxSum = 0;
+        int maxSum = nums[0];  //用于保存最大子序列和
         for (int i = 0; i < nums.length; i++) {
-            int sum = nums[i];
-            if (sum > maxSum) {
-                maxSum = sum;
-            }
-            int j = i + 1;
-            while (j < nums.length) {
-                sum += nums[j];
-                if (sum > maxSum) {
-                    maxSum = sum;
-                }
-                if(sum <= 0){
-                    break;
-                }
-                j++;
-            }
+            preMaxSum = Math.max(preMaxSum + nums[i],nums[i]);
+            maxSum = Math.max(maxSum,preMaxSum);
         }
         return maxSum;
     }
