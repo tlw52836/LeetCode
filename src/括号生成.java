962ef07a76a3c1e6b935933e11f58b80a1d4d447
tlw52836 <1660872260@qq.com>
2021-09-30 17:27:09 +0800

    LeetCode习题

---
+++ src/括号生成.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class 括号生成 {
+    public static void main(String[] args) {
+        char[] chs = new char[10];
+       for(char ch:chs){
+           System.out.println(ch);
+       }
+    }
+    public static List<String> generateParenthesis(int n) {
+         return null;
+    }
+}
