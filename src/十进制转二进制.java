d5351516c4dacfaaa76b85e343f2ed98c297cd89
tlw52836 <1660872260@qq.com>
2021-10-22 17:17:21 +0800

    LeetCode习题

---
+++ src/十进制转二进制.java
@@ -0,0 +1,19 @@
+public class 十进制转二进制 {
+    public static void main(String[] args) {
+        System.out.println(transform(42));
+    }
+
+    /**
+     * 正整数转换为二进制
+     * @param num
+     * @return
+     */
+    public static String transform(int num){
+        String s = "";
+        while (num != 0) {
+            s = (num % 2) + s;
+            num = num / 2;
+        }
+        return s;
+    }
+}
