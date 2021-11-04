a7fe3d8749dbc4bf7dabaaf5f69190ab4de834e5
tlw52836 <1660872260@qq.com>
2021-11-04 14:43:11 +0800

    LeetCode习题

---
+++ src/全排列_回溯.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class 全排列_回溯 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        System.out.println(new Solution().permute(nums));
+
+    }
+
+    static class Solution{
+        private int[] nums;
+        private int[] visit;  //记录是否访问过
+        private List<List<Integer>> results;  //保存结果
+
+        /**
+         * dfs + 回溯
+         * @param nums
+         * @return
+         */
+        public List<List<Integer>> permute(int[] nums) {
+            this.nums = nums;
+            this.visit = new int[nums.length];
+            this.results = new ArrayList<>();
+
+            //用于存放排列结果
+            for (int i = 0; i < nums.length; i++) {
+                visit[i] = 1;
+                List<Integer> list = new ArrayList<>();  //保存目前访问过的路径
+                list.add(nums[i]);
+
+                dfs(0,list);
+
+                /**
+                 * 回溯
+                 */
+                list.remove(list.size()-1);
+                visit[i] = 0;
+            }
+            return results;
+        }
+
+        public void dfs(int depth, List list) {
+            //System.out.println("list:" + list);
+            if(depth == nums.length-1) {
+                results.add(new ArrayList<>(list));
+                return;
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                /**
+                 * 剪枝
+                 */
+                if (visit[i] == 1) {
+                    continue;
+                } else {
+                    visit[i] = 1;
+                    list.add(nums[i]);
+                    dfs(depth+1, list);
+                    /**
+                     * 回溯
+                     */
+                    list.remove(list.size()-1);
+                    visit[i] = 0;
+                }
+            }
+        }
+    }
+}
