d899ac68589f9065b2ce230a457d523d35701c9a
taoliwei52836 <1660872260@qq.com>
2021-11-07 17:06:25 +0800

    modify

--- src/最长回文子串.java
+++ src/最长回文子串.java
@@ -1,26 +1,54 @@
+import java.util.*;
+
 public class 最长回文子串 {
     public static void main(String[] args) {
-        String s = "cbbd";
-        System.out.println(longestPalindrome(s));
+        String s = "a";
+        System.out.println("result:" + longestPalindrome(s));
     }
 
+    /**
+     * 动态规划：
+     * f(i) 表示s[0...i]中最长回文子串，f(i+1) = Math.length(f(i),is_Palindrome(s[0...i+1]),is_Palindrome(s[1...i+]),is_Palindrome(s[2...i+1])...)
+     * 其中s[0...i+1],s[1...i+],s[2...i+1]...即新添加的字串
+     * 二位数组dp[i][j]用于标记s[i...j]是否为回文串
+     * @param s
+     * @return
+     */
     public static String longestPalindrome(String s) {
+        int len = s.length();
+        int[][] dp = new int[len][len];  //标记s[i..j]是否为回文串
+        //记录最长回文串的开始与结束序号
+        int begin = 0, end = 0;
+        int l = 0;
 
-        String str = ""; //记录最长回文串
-        for(int i = 0;i < s.length();i++){
-            int m = i,n = s.length() -1;
-            while (m < n) {
-                if(s.charAt(m) == s.charAt(n)){
-                    m++;
-                    n--;
-                }else {
-                    break;
+        char[] ch = s.toCharArray();
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (ch[i] != ch[j]) {
+                    continue;
+                } else {
+                    if (i-j <= 2 || dp[j+1][i-1] == 1) {
+                        if (i-j+1 > l) {
+                            begin = j;
+                            end = i;
+                            l = i-j+1;
+                        }
+                        dp[j][i] = 1;
+                        System.out.println("---------------");
+                        System.out.println("i = " + i + " j = " + j);
+                        System.out.println("begin = " + begin + " end = " + end);
+                        for (int k = 0; k < len; k++) {
+                            System.out.println(Arrays.toString(dp[k]));
+                        }
+
+                    } else {
+                        continue;
+                    }
                 }
             }
-            if(m >= n && (m-n+1) > str.length()){
-                str = s.substring(m,n+1);
-            }
         }
-        return str;
+
+        return s.substring(begin, end+1);
     }
 }
---
+++ src/比特位计数.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class 比特位计数 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(countBits(5)));
+    }
+
+    /**
+     * 奇数：二进制表示中，奇数一定比前面那个偶数多一个 1，因为多的就是最低位的 1
+     *偶数：二进制表示中，偶数中 1 的个数一定和除以 2 之后的那个数一样多。因为最低位是 0，除以 2 就是右移一位，也就是把那个 0 抹掉而已，所以 1 的个数是不变的
+     * @param n
+     * @return
+     */
+    public static int[] countBits(int n) {
+        int[] res = new int[n+1];
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 == 0) {
+                res[i] = res[i/2];
+             } else{
+                res[i] = res[i-1]+1;
+            }
+        }
+        return res;
+    }
+}
