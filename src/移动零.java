44eb0f04f7119b58250ed0a1675873fed1d71dc0
taoliwei52836 <1660872260@qq.com>
2021-11-07 09:06:14 +0800

    modify

--- src/移动零.java
+++ src/移动零.java
@@ -7,18 +7,19 @@ public class 移动零 {
         s.moveZeroes(nums);
         System.out.println(Arrays.toString(nums));
     }
-}
-class Solution {
-    public void moveZeroes(int[] nums) {
-        int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i] != 0){
-                nums[j++] = nums[i];
-            }
-        }
 
-        while (j < nums.length) {
-            nums[j++] = 0;
+    static class Solution {
+        public void moveZeroes(int[] nums) {
+            int j = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if(nums[i] != 0){
+                    nums[j++] = nums[i];
+                }
+            }
+
+            while (j < nums.length) {
+                nums[j++] = 0;
+            }
         }
     }
 }
