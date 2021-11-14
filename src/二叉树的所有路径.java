66f515fd770b84b4e5418d9534c36a07fdf52cb8
taoliwei52836 <1660872260@qq.com>
2021-11-14 08:58:41 +0800

    modify

---
+++ src/二叉树的所有路径.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class 二叉树的所有路径 {
+    public static void main(String[] args) {
+        TreeNode root = TreeNode.build(new Integer[]{1, 2, 3, null, 5});
+
+        System.out.println(new Solution().binaryTreePaths(root));
+    }
+    static class Solution {
+        private List<String> list;
+
+        public List<String> binaryTreePaths(TreeNode root) {
+            list = new ArrayList<>();
+            dfs(root, "");
+
+            return list;
+        }
+
+        public void dfs (TreeNode root, String str) {
+            if (root.left == null && root.right == null) {
+                str += root.val;
+                list.add(str);
+                return;
+            } else {
+                str += root.val + "->";
+            }
+
+            if (root.left != null)
+                dfs(root.left, str);
+
+            if (root.right != null)
+                dfs(root.right, str);
+        }
+    }
+
+}
---
+++ src/赎金信.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class 赎金信 {
+    public static void main(String[] args) {
+        String ransomNote = "dehifb";
+        String magazine = "hhjdgjbhahaagihhhhhajjibjffhijehda";
+        System.out.println(canConstruct(ransomNote, magazine));
+    }
+
+//    public static boolean canConstruct(String ransomNote, String magazine) {
+//        Map<Character, Integer> map = new HashMap<>();
+//        for (int i = 0; i < magazine.length(); i++) {
+//            if (!map.containsKey(magazine.charAt(i))) {
+//                map.put(magazine.charAt(i), 1);
+//            } else {
+//                int val = map.get(magazine.charAt(i));
+//                map.put(magazine.charAt(i), val+1);
+//            }
+//        }
+//
+////        for (Map.Entry<Character, Integer> m:map.entrySet()) {
+////            System.out.println(m.getKey() + "  " + m.getValue());
+////        }
+//
+//        for (int i = 0; i < ransomNote.length(); i++) {
+//            if (!map.containsKey(ransomNote.charAt(i)) || map.get(ransomNote.charAt(i)) == 0) {
+//                return false;
+//            } else {
+//                int val = map.get(ransomNote.charAt(i));
+//                map.put(ransomNote.charAt(i), val-1);
+//            }
+//        }
+//        return true;
+//    }
+
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        int[] nums = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            nums[magazine.charAt(i)-'a']++;
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (nums[ransomNote.charAt(i)-'a'] <= 0) {
+                return false;
+            } else {
+                nums[ransomNote.charAt(i)-'a']--;
+            }
+        }
+        return true;
+    }
+}
