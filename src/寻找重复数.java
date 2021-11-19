2ae210b93d27bb25788088b8f36118de5b1e5b2b
tlw52836 <1660872260@qq.com>
2021-11-19 13:32:58 +0800

    LeetCode习题

---
+++ src/寻找重复数.java
@@ -0,0 +1,18 @@
+//import java.util.Arrays;
+//
+//public class 寻找重复数 {
+//    public static void main(String[] args) {
+//        int[] nums = {1,3,4,2,2};
+//        System.out.println(findDuplicate(nums));
+//    }
+//    public static int findDuplicate(int[] nums) {
+//        int l = 1, r = nums.length-1;
+//
+//        while (l <= r)
+//
+//
+//
+//
+//        return -1;
+//    }
+//}
