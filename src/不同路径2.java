da89ebbe6a5367e4b06b02ac54b8760617c28c8f
tlw52836 <1660872260@qq.com>
2021-12-08 17:30:59 +0800

    LeetCode习题

--- src/不同路径2.java
+++ src/不同路径2.java
@@ -1,30 +1,31 @@
+import java.util.Arrays;
+
 public class 不同路径2 {
     public static void main(String[] args) {
-        System.out.println(f(16));
-        System.out.println(f(9));
-        System.out.println(uniquePaths(10, 10));
+        uniquePaths(3, 4);
     }
 
     public static int uniquePaths(int m, int n) {
-        if (m == 1 || n == 1) {
-            return 1;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 1;
+                else
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
         }
 
-        m--;
-        n--;
-        return f(m+n) / (f(n) * f(m));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        return 0;
     }
 
-    /**
-     * 求n的阶乘
-     * @param n
-     * @return
-     */
-    public static int f(int n) {
-        if (n == 1) {
-            return 1;
-        }
-        return n * f(n-1);
-    }
+
 
 }
