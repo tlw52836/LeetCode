ca9d664dcc645889275e2ace052e5fddcd22a8ff
tlw52836 <1660872260@qq.com>
2021-12-13 20:04:29 +0800

    LeetCode习题

--- src/螺旋矩阵.java
+++ src/螺旋矩阵.java
@@ -3,10 +3,10 @@ import java.util.List;
 
 public class 螺旋矩阵 {
     public static void main(String[] args) {
-
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println(spiralOrder(matrix));
     }
-
-    public List<Integer> spiralOrder(int[][] matrix) {
+    public static List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> res = new ArrayList<>();
         int[][] direct = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         int row = matrix.length;
@@ -16,11 +16,17 @@ public class 螺旋矩阵 {
         int i = 0, j = 0, k = 0;
         while (true) {
             res.add(matrix[i][j]);
-            while (i+direct[k][0] < 0 || i+direct[k][0] >= row || j+direct[k][1] < 0 || j+direct[k][1] >= col || visit[i+direct[i][0]][j+direct[i][1]] == true) {
+            int t = 0;
+            while (i+direct[k][0] < 0 || i+direct[k][0] >= row || j+direct[k][1] < 0 || j+direct[k][1] >= col || visit[i+direct[k][0]][j+direct[k][1]] == true) {
                 k = (k + 1) % 4;
+                t++;
+                if (t == 3)
+                    break;
             }
+            i = i+direct[k][0];
+            j = j+direct[k][1];
         }
-
-        return res;
+        return null;
     }
+
 }
