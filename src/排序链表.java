7b88af66bbe9c793771f71ba803ce1cb0f0d65c9
tlw52836 <1660872260@qq.com>
2021-12-14 20:06:32 +0800

    LeetCode习题

--- src/排序链表.java
+++ src/排序链表.java
@@ -1,2 +1,25 @@
 public class 排序链表 {
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode sortList(ListNode head) {
+
+        sort()
+        return null;
+    }
+
+    public static ListNode sort(ListNode l, ListNode r) {
+        ListNode slow, fast;
+        slow = fast = l;
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode mid = slow;
+        sort(l, mid);
+        sort(mid.next, r)
+    }
 }
