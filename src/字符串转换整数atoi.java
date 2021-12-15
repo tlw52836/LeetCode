faec75bbdfa4f25e44056b1b8dec70decb73da3b
tlw52836 <1660872260@qq.com>
2021-12-15 19:55:49 +0800

    LeetCode习题

--- src/字符串转换整数atoi.java
+++ src/字符串转换整数atoi.java
@@ -15,59 +15,56 @@ public class 字符串转换整数atoi {
             i++;
         }
 
-        String res = "";
-        if (i < ss.length && ss[i] == '-') {
-            res += "-";
-            i++;
-
-            boolean flag = true;
-
-            while (i < ss.length && Character.isDigit(ss[i])) {
-                if (ss[i] == '0' && flag) {
-                    i++;
-                } else {
-                    flag = false;
-                    res += ss[i];
-                    i++;
-                }
-            }
-
-            if (res.length() <= 1)
-                return 0;
-
-
-            BigInteger num = new BigInteger(res);
-            BigInteger m = new BigInteger(Integer.MIN_VALUE+"");
-            num.compareTo(m);
-
-            return num.compareTo(m) == -1 ? Integer.MIN_VALUE : Integer.valueOf(res);
-        } else if (i < ss.length && (ss[i] == '+' || Character.isDigit(ss[i]))) {
-            if (ss[i] == '+')
-                i++;
-
-            boolean flag = true;
-
-            while (i < ss.length && Character.isDigit(ss[i])) {
-                if (ss[i] == '0' && flag) {
-                    i++;
-                } else {
-                    flag = false;
-                    res += ss[i];
-                    i++;
-                }
-            }
-
-            if (res.length() <= 0)
-                return 0;
-
-            BigInteger num = new BigInteger(res);
-            BigInteger m = new BigInteger(Integer.MAX_VALUE+"");
-            num.compareTo(m);
-
-            return num.compareTo(m) == 1 ? Integer.MAX_VALUE : Integer.valueOf(res);
-        } else {
+        //检查第一个字符
+        if (i >= ss.length || !(ss[i] == '-' || ss[i] == '+' || Character.isDigit(ss[i])))
             return 0;
+
+        String res = "";
+        
+        //添加正负号
+        if (ss[i] == '-') {
+            res += '-';
+            i++;
         }
+
+        if (ss[i] == '+') {
+            i++;
+        }
+
+        //添加数字
+        boolean flag = true;
+
+        while (i < ss.length && Character.isDigit(ss[i])) {
+            if (ss[i] == '0' && flag) {
+                i++;
+            } else {
+                flag = false;
+                res += ss[i];
+                i++;
+            }
+        }
+
+        if (res.equals("-"))
+            return 0;
+
+        String inter;
+        if (res.charAt(0) == '-') {
+            inter = (Integer.MIN_VALUE + "").substring(1);
+        } else {
+            inter = Integer.MAX_VALUE + "";
+        }
+
+        if (res.length() < inter.length())
+            return Integer.valueOf(res);
+        else if (inter.length() >)
+
+        for (int j = 0; j < ; j++) {
+
+        }
+
+
+
+        return -1;
     }
 
 }
