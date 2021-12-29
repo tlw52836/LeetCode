41d9cf2067c7c2b790e5c6df803eac68a8400c48
tlw52836 <1660872260@qq.com>
2021-12-29 09:38:53 +0800

    LeetCode习题

--- src/任务调度器.java
+++ src/任务调度器.java
@@ -6,7 +6,9 @@ import java.util.Map;
 public class 任务调度器 {
 
     public static void main(String[] args) {
-
+        char[] task = {'A','A','A','B','B','B'};
+        int n = 2;
+        System.out.println(leastInterval(task, n));
     }
     public static int leastInterval(char[] tasks, int n) {
         Map<Character, Integer> maps = new HashMap<>();
@@ -15,11 +17,19 @@ public class 任务调度器 {
         }
 
         List<Integer> list = new ArrayList<>();
-        int maxIndex = -1;
-        for (int i = 0; i < maps.size(); i++) {
-            list.add(maps.get(i));
+        int max = -1;
+        int all = 0;
+
+        for (int m:maps.values()) {
+            max = Math.max(max, m);
+            all += m;
+            list.add(m);
         }
 
-        return 0;
+        System.out.println(max);
+        if (all-((max-1)*(n+1)+1) >= 0)
+            return all;
+        else
+            return (max-1) * (n+1) + 1;
     }
 }
