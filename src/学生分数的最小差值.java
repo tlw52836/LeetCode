6d5f6bbeb333f730e50bf24132c73e235f0f13dc
tlw52836 <1660872260@qq.com>
2022-02-11 18:17:32 +0800

    LeetCode习题

---
+++ src/学生分数的最小差值.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class 学生分数的最小差值 {
+    public static void main(String[] args) {
+
+    }
+
+    public int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        return 0;
+    }
+}
