8d4e846b12f96dc546854724e04f02c359ed615e
taoliwei52836 <1660872260@qq.com>
2022-02-15 22:16:33 +0800

    提交 学生分数的最小差值

--- src/学生分数的最小差值.java
+++ src/学生分数的最小差值.java
@@ -2,12 +2,24 @@ import java.util.Arrays;
 
 public class 学生分数的最小差值 {
     public static void main(String[] args) {
-
+        int[] nums = {9, 4, 1, 7};
+        int k = 2;
+        int res = minimumDifference(nums, k);
+        System.out.println(res);
     }
 
-    public int minimumDifference(int[] nums, int k) {
+    public static int minimumDifference(int[] nums, int k) {
         Arrays.sort(nums);
+        int i = 0;
+        int j = k - 1;
 
-        return 0;
+        int res = Integer.MAX_VALUE;
+        while (j < nums.length) {
+            if (nums[j] - nums[i] < res)
+                res = nums[j] - nums[i];
+            i++;
+            j++;
+        }
+        return res;
     }
 }
