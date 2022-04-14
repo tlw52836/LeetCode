e12b2dc878b7c9b41431c657ef09fc21feb219c2
tlw52836 <1660872260@qq.com>
2022-04-14 19:35:15 +0800

    最长递增子序列

--- src/初级算法/树/验证二叉搜索树.java
+++ src/初级算法/树/验证二叉搜索树.java
@@ -3,38 +3,38 @@ package 初级算法.树;
 public class 验证二叉搜索树 {
     public static void main(String[] args) {
         TreeNode root = TreeNode.build(new Integer[]{5,1,4,null,null,3,6});
-        System.out.println(Integer.MIN_VALUE);
         Solution s = new Solution();
         System.out.println(s.isValidBST(root));
     }
-}
-
-class Solution {
-    private Integer preNum = null;
-    private boolean res = true;
+    static class Solution {
+        private long preNum = Long.MIN_VALUE;
+        private boolean res = true;
 
 
 
-    public boolean isValidBST(TreeNode root) {
-        inOrder(root);
-        return res;
-    }
+        public boolean isValidBST(TreeNode root) {
+            inOrder(root);
+            return res;
+        }
 
-    /**
-     * 树的中序遍历变形
-     * @param root
-     */
-    public void inOrder(TreeNode root) {
-        if (root != null) {
-            inOrder(root.left);
-            if (preNum != null && preNum >= root.val) {
-                res = false;
-                //剪枝
-                return;
-            } else {
-                preNum = root.val;
+        /**
+         * 树的中序遍历变形
+         * @param root
+         */
+        public void inOrder(TreeNode root) {
+            if (root != null) {
+                inOrder(root.left);
+                if (preNum >= root.val) {
+                    res = false;
+                    //剪枝
+                    return;
+                } else {
+                    preNum = root.val;
+                }
+                inOrder(root.right);
             }
-            inOrder(root.right);
         }
     }
 }
+
+
