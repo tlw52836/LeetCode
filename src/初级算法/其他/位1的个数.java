479ee49418cdabc03b64056bce4ad3e967be7505
tlw52836 <1660872260@qq.com>
2022-05-10 11:10:15 +0800

    异或运算

--- src/初级算法/其他/位1的个数.java
+++ src/初级算法/其他/位1的个数.java
@@ -7,7 +7,7 @@ public class 位1的个数 {
     public static int hammingWeight(int n) {
         int res = 0;
         for (int i = 0; i < 32; i++) {
-            if ((n & 1 << i)  != 0)
+            if ((n & (1 << i))  != 0)
                 res++;
         }
         return res;
