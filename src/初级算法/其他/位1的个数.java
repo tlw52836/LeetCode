7ad8ebbdfc818e60a1c4071c560805d404516720
tlw52836 <1660872260@qq.com>
2022-05-10 11:50:18 +0800

    位运算

--- src/初级算法/其他/位1的个数.java
+++ src/初级算法/其他/位1的个数.java
@@ -2,12 +2,23 @@ package 初级算法.其他;
 
 public class 位1的个数 {
     public static void main(String[] args) {
-        System.out.println(hammingWeight(5));
+        System.out.println(1<<31);
+        //System.out.println(hammingWeight(5));
     }
     public static int hammingWeight(int n) {
         int res = 0;
         for (int i = 0; i < 32; i++) {
             if ((n & (1 << i))  != 0)
+                res++;
+        }
+        return res;
+    }
+
+    public static int hammingWeight2(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int t = n >> i;
+            if ((t & 1) == 1)
                 res++;
         }
         return res;
--- src/初级算法/其他/颠倒二进制位.java
+++ src/初级算法/其他/颠倒二进制位.java
@@ -2,21 +2,17 @@ package 初级算法.其他;
 
 public class 颠倒二进制位 {
     public static void main(String[] args) {
-        System.out.println(Integer.valueOf("10000001011010111110000101100011",2));
-        //System.out.println(reverseBits(43261596));
+        int num = 3;
+
     }
 
     public static int reverseBits(int n) {
-        String s = "";
+        int res = 0;
         for (int i = 0; i < 32; i++) {
-            if((n & 1 << i) != 0) {
-                s = "0" + s;
-            } else {
-                s = "1" + s;
-            }
+            int t = (n >> i) & 1;
+            res = (res << 1) | t;
         }
-        System.out.println(s);
-        Integer res = Integer.valueOf(s, 2);
+
         return res;
     }
 
