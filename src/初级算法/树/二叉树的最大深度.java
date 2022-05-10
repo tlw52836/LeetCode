c4603e021464b1297a96fa91f99356469e117592
tlw52836 <1660872260@qq.com>
2022-05-10 14:15:41 +0800

    位运算

--- src/初级算法/树/二叉树的最大深度.java
+++ src/初级算法/树/二叉树的最大深度.java
@@ -6,10 +6,10 @@ public class 二叉树的最大深度 {
         System.out.println(maxDepth(root));
     }
     public static int maxDepth (TreeNode root){
-        if (root == null) {
-            return 0;
-        } else {
+        if (root != null) {
             return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
         }
+
+        return 0;
     }
 }
