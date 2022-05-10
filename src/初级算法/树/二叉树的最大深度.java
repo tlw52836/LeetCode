81588004f69a64b38cf6f40715eef19974a505ae
tlw52836 <1660872260@qq.com>
2022-05-10 15:39:09 +0800

    树的层次遍历

--- src/初级算法/树/二叉树的最大深度.java
+++ src/初级算法/树/二叉树的最大深度.java
@@ -6,10 +6,10 @@ public class 二叉树的最大深度 {
         System.out.println(maxDepth(root));
     }
     public static int maxDepth (TreeNode root){
-        if (root != null) {
-            return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        if (root != null) {  //当前结点不为空
+            return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;  //结点不为空，高度=子树高度较大者+1
         }
 
-        return 0;
+        return 0;  //节点为空，高度为零
     }
 }
---
+++ src/初级算法/树/对称二叉树.java
@@ -0,0 +1,44 @@
+package 初级算法.树;
+
+public class 对称二叉树 {
+    public static void main(String[] args) {
+        TreeNode root = TreeNode.build(new Integer[]{1, 2, 2, 3, 4, 4, 3});
+        System.out.println(isSymmetric(root));
+    }
+    public static boolean isSymmetric(TreeNode root) {
+
+        return dfs(root.left, root.right);
+    }
+
+    public static boolean dfs(TreeNode root1, TreeNode root2) {
+
+        /**
+         * 结点都为null
+         */
+        if ((root1 == null && root2 == null)) {
+            return true;
+        }
+
+        /**
+         * 一个结点为null
+         */
+        if (root1 == null || root2 == null ) {
+            return false;
+        }
+
+        /**
+         * 两个值不相等
+         */
+        if (root1.val != root2.val) {
+            return false;
+        }
+
+        /**
+         * 两个值相等
+         */
+        boolean res1 = dfs(root1.left, root2.right);
+        boolean res2 = dfs(root1.right, root2.left);
+
+        return (res1 && res2);
+    }
+}
